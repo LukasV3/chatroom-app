e128f2502e6d8c4f9c916bbcecaea5d7b9e0d740
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-04-05 08:25:55 +0100

    navbar is now mobile responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <nav className="navbar">
-      <h1 className="navbar__logo">
-        Chatroom
-        <i className="fab fa-rocketchat"></i>
-      </h1>
+    <div className="navbar">
+      <nav className="navbar__nav">
+        <h1 className="navbar__logo">
+          Chatroom
+          <i className="fab fa-rocketchat"></i>
+        </h1>
 
-      <div className="navbar__links--icon">
-        <i className="fas fa-bars"></i>
-      </div>
+        <div className="navbar__links--icon" onClick={() => setClicked(!clicked)}>
+          {clicked ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+        </div>
 
-      <ul className="navbar__links">
-        <li className="navbar__item">
-          <Link to="/login" className="navbar__link">
-            Login
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link to="/signup" className="navbar__link">
-            SignUp
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className={clicked ? "navbar__links active" : "navbar__links"}>
+          <li className="navbar__item">
+            <Link to="/login" className="navbar__link">
+              Login
+            </Link>
+          </li>
+          <li className="navbar__item">
+            <Link to="/signup" className="navbar__link">
+              SignUp
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
