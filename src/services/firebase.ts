9e65e8285f89b4b7b18643f6b4d26710352bb1f9
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-04-06 10:22:29 +0100

    initialized firebase db and auth iusing REACT_APP .env vars

---
+++ src/services/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_MSID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID,
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const db = firebase.database();
